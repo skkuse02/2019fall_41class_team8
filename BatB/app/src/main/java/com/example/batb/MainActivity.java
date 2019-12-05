8179d7b6e7adb7052119166b9fd65c56499b3be7
jyooj08 skkusosc <jyooj08@naver.com>
2019-12-06 05:12:55 +0900

    Complement the camera and album functions

--- BatB/app/src/main/java/com/example/batb/MainActivity.java
+++ BatB/app/src/main/java/com/example/batb/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.batb;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import android.Manifest;
+import android.app.Activity;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -31,8 +33,8 @@ import java.util.Date;
 public class MainActivity extends AppCompatActivity {
     private int PHOTO_FROM_CAMERA=0, PHOTO_FROM_ALBUM=1;
     private Button cameraButton, albumButton, quitButton, helpButton;
-    ImageView imageView;
     Button button;
+    Uri photoURI;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,12 +43,63 @@ public class MainActivity extends AppCompatActivity {
 
         tedPermission();
 
-        openCamera();
-        openAblum();
-        quit();
-        help();
+        cameraButton = (Button) findViewById(R.id.camera);
+        albumButton = (Button) findViewById(R.id.album);
+        quitButton = (Button) findViewById(R.id.quit);
+        helpButton = findViewById(R.id.info);
 
-        imageView = (ImageView)findViewById(R.id.logo);
+        cameraButton.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                //Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                //startActivityForResult(intent,PHOTO_FROM_CAMERA);
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                // Ensure that there's a camera activity to handle the intent
+                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                    // Create the File where the photo should go
+                    File photoFile = null;
+                    try {
+                        photoFile = createImageFile();
+                    } catch (IOException ex) {}
+                    // Continue only if the File was successfully created
+                    if (photoFile != null) {
+                        photoURI = FileProvider.getUriForFile(v.getContext(),
+                                getPackageName(),
+                                photoFile);
+                        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                        startActivityForResult(takePictureIntent, PHOTO_FROM_CAMERA);
+                    }
+                }
+            }
+        });
+
+        albumButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+                startActivityForResult(intent,PHOTO_FROM_ALBUM);
+            }
+        });
+
+        quitButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finishActivity(0);
+                System.runFinalization();
+                System.exit(0);
+            }
+        });
+
+        helpButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), HelpActivity.class);
+                startActivity(intent);
+            }
+        });
+
         button = findViewById(R.id.button2);
         button.setOnClickListener(new OnClickListener() {
             @Override
@@ -77,81 +130,37 @@ public class MainActivity extends AppCompatActivity {
                 .check();
     }
 
-    private void openCamera() {
-        cameraButton = (Button) findViewById(R.id.camera);
-        cameraButton.setOnClickListener(new OnClickListener() {
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
 
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(intent,PHOTO_FROM_CAMERA);
-            }
-        });
-    }
-
-    private void openAblum(){
-        albumButton = (Button) findViewById(R.id.album);
-        albumButton.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_PICK);
-                intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
-                startActivityForResult(intent,PHOTO_FROM_ALBUM);
-            }
-        });
-    }
-
-    private void quit(){
-        quitButton = (Button) findViewById(R.id.quit);
-        quitButton.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finishActivity(0);
-                System.runFinalization();
-                System.exit(0);
-            }
-        });
-    }
-
-    private void help(){
-        helpButton = findViewById(R.id.info);
-        helpButton.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(view.getContext(), HelpActivity.class);
-                startActivity(intent);
-            }
-        });
+        return image;
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode != Activity.RESULT_OK){
+            Toast.makeText(this,"취소되었습니다.",Toast.LENGTH_SHORT).show();
+            return;
+        }
         if(requestCode==PHOTO_FROM_ALBUM){
             Uri uri = data.getData();
-            Cursor cursor = null;
-            String filepath;
-
-            try{
-                String[] proj={MediaStore.Images.Media.DATA};
-                cursor = getContentResolver().query(uri,proj,null,null,null);
-                int col_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-                cursor.moveToFirst();
-                filepath = cursor.getString(col_index);
-            }finally {
-                if(cursor!= null) cursor.close();
-            }
 
             Intent intent = new Intent(this, PhotoCheckActivity.class);
-            intent.putExtra("code",PHOTO_FROM_ALBUM);
-            intent.putExtra("file",filepath);
+            intent.putExtra("uri",uri);
             startActivity(intent);
         }
         else if (requestCode==PHOTO_FROM_CAMERA){
-            Bundle bundle = data.getExtras();
             Intent intent = new Intent(this, PhotoCheckActivity.class);
-            intent.putExtra("code",PHOTO_FROM_CAMERA);
-            intent.putExtra("bundle",bundle);
+            intent.putExtra("uri",photoURI);
             startActivity(intent);
 
         }
--- BatB/app/src/main/java/com/example/batb/PhotoCheckActivity.java
+++ BatB/app/src/main/java/com/example/batb/PhotoCheckActivity.java
@@ -20,19 +20,8 @@ public class PhotoCheckActivity extends AppCompatActivity {
         setContentView(R.layout.activity_photo_check);
 
         imageView = (ImageView)findViewById(R.id.imageView);
-        int code = getIntent().getIntExtra("code",-1);
-
-        if(code == 1){ // album
-            String filepath = getIntent().getStringExtra("file");
-            tempFile = new File(filepath);
-            BitmapFactory.Options options = new BitmapFactory.Options();
-            Bitmap bitmap = BitmapFactory.decodeFile(tempFile.getAbsolutePath(), options);
-            imageView.setImageBitmap(bitmap);
-        }else if (code == 0){ // camera
-            Bundle bundle = getIntent().getBundleExtra("bundle");
-            Bitmap bitmap = (Bitmap)bundle.get("data");
-            imageView.setImageBitmap(bitmap);
-        }
+        Uri uri = getIntent().getParcelableExtra("uri");
+        imageView.setImageURI(uri);
 
     }
 }
