da60626cb1d479daeb587c34295ec18084e5ebc5
lojpark <lojpark@gmail.com>
2019-12-07 15:52:43 +0900

    Create RequestActivity

--- BatB/app/src/main/java/com/example/batb/PhotoCheckActivity.java
+++ BatB/app/src/main/java/com/example/batb/PhotoCheckActivity.java
@@ -2,16 +2,21 @@ package com.example.batb;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import java.io.File;
 
 public class PhotoCheckActivity extends AppCompatActivity {
     private ImageView imageView;
+    private Button yesButton;
+    private Button noButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,5 +26,22 @@ public class PhotoCheckActivity extends AppCompatActivity {
         Uri uri = getIntent().getParcelableExtra("uri");
         imageView.setImageURI(uri);
 
+        yesButton = (Button)findViewById(R.id.yesButton);
+        noButton = (Button)findViewById(R.id.nobutton);
+
+        yesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), RequestActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        noButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
---
+++ BatB/app/src/main/java/com/example/batb/RequestActivity.java
@@ -0,0 +1,16 @@
+package com.example.batb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RequestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_request);
+
+
+    }
+}
