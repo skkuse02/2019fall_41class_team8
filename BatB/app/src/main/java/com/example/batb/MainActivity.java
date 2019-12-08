5a3bb33ae76fca37d4ce62a960feadbf22a7a165
lojpark <lojpark@gmail.com>
2019-12-09 03:52:58 +0900

    Save the madeup image

--- BatB/app/src/main/java/com/example/batb/MainActivity.java
+++ BatB/app/src/main/java/com/example/batb/MainActivity.java
@@ -222,6 +222,4 @@ public class MainActivity extends AppCompatActivity {
         return image;
     }
 
-
-
 }
--- BatB/app/src/main/java/com/example/batb/ResultActivity.java
+++ BatB/app/src/main/java/com/example/batb/ResultActivity.java
@@ -10,16 +10,20 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Objects;
@@ -68,6 +72,17 @@ public class ResultActivity extends AppCompatActivity {
                 }
             }
         });
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    createImageFile();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
     @SuppressLint("StaticFieldLeak")
@@ -91,19 +106,31 @@ public class ResultActivity extends AppCompatActivity {
         }
     }
 
-    private File createImageFile() throws IOException {
+    private void createImageFile() throws IOException {
 
-        // 이미지 파일 이름 ( blackJin_{시간}_ )
+        // 이미지 파일 이름
         String timeStamp = new SimpleDateFormat("HHmmss").format(new Date());
-        String imageFileName = "BatB_" + timeStamp + "_";
+        String imageFileName = "BatB_" + timeStamp + ".jpg";
+        String path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM) + "/BatB/";
 
-        // 이미지가 저장될 폴더 이름 ( blackJin )
-        File storageDir = new File(Environment.DIRECTORY_DCIM + "/BatB/");
+        // 이미지가 저장될 폴더 이름
+        File storageDir = new File(path);
         if (!storageDir.exists()) storageDir.mkdirs();
 
-        // 빈 파일 생성
-        File image = File.createTempFile(imageFileName, ".jpg", storageDir);
 
-        return image;
+        File file = new File(path, imageFileName);
+        OutputStream fOut = new FileOutputStream(file);
+        madeupImage.compress(Bitmap.CompressFormat.JPEG, 100, fOut);
+        fOut.close();
+
+        MediaStore.Images.Media.insertImage(getContentResolver(),file.getAbsolutePath(),file.getName(),file.getName());
+
+        Toast toast = Toast.makeText(getApplicationContext(), path + " 에 저장 되었습니다.", Toast.LENGTH_SHORT);
+        toast.show();
+
+        // 빈 파일 생성
+        //File image = File.createTempFile(imageFileName, ".jpg", storageDir);
+
+        //return image;
     }
 }
